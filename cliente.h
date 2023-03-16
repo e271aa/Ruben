dcbde1f6cc72f1d5c19c392264ad796e15bd203c
Ruben Martins <120483367+e271aa@users.noreply.github.com>
2023-03-16 21:48:40 +0000

    Add files via upload

--- cliente.h
+++ cliente.h
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-typedef struct cl_registo
+typedef struct cliente_registo
 {
     int numero_cliente;
     char nome[50];
     char email[50];
     char telefone[15];
     char morada[100];
+    struct cliente_registo * seguinte;
 } Cliente;
 
-Cliente* inserirClientes(Cliente* inicio, int numero_cliente, char nome[], char email[], char telefone[15], char morada[100];); // Inserção de um novo registo
+Cliente* inserirClientes(Cliente* inicio, int numcliente, char nome[], char email[], char tel[15], char morada[100];); // Inserção de um novo registo
 void listarClientes(Cliente* inicio); // listar na consola o conteúdo da lista ligada
-int existeClientes(Cliente* inicio, int numero_cliente;); // Determinar existência do 'codigo' na lista ligada 'inicio'
-Cliente* removerClientes(Cliente* inicio, int numero_cliente;); // Remover um Cliente a partir do seu código
+int existeClientes(Cliente* inicio, int numcliente;); // Determinar existência do 'codigo' na lista ligada 'inicio'
+Cliente* removerClientes(Cliente* inicio, int numcliente;); // Remover um Cliente a partir do seu código
 
 int guardarClientes(Cliente* inicio);
 Cliente* lerClientes();
--- fase1.c
+++ fase1.c
@@ -1,48 +1,86 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <windows.h>
 #include "meio.h"
 #include "gestor.h"
 #include "cliente.h"
 
-int menu()
+void loading()
 {
-    int op;
-	printf("\n\n\t\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd Bem-vindo(a) a GreenGo! \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
-	printf("\tA GreenGo é uma empresa de mobilidade elétrica que oferece soluções de transporte sustentável para pessoas e empresas. Com a nossa frota de veículos elétricos, ajudamos a reduzir as emissões de CO2 e a preservar o meio ambiente.\n\n\n\n\n\n");
-	printf("\t2023 - Todos os direitos reservados")
-	Sleep(1000);
+	printf("\033[32m\n\n\t\t\t\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd Bem-vindo(a) a GreenGo! \xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\033[0m\n");
+	printf("\nA GreenGo e uma empresa de mobilidade eletrica que oferece solucoes de transporte sustentavel para pessoas e empresas. \nCom a nossa frota de veiculos eletricos, ajudamos a reduzir as emissoes de CO2 e a preservar o meio ambiente.\n\n\n");
+	printf("2023 - Todos os direitos reservados");
+	Sleep(8000);
     system("cls");
+}
+
+int menu_principal()
+{
+	loading();
+    int op_p;
 	printf("\n\n\t=====================================\n");
     printf("\t|          Menu  Principal          |\n");
     printf("\t=====================================\n");
     printf("\t| 1. Meio de mobilidade eletrica    |\n");
     printf("\t| 2. Cliente                        |\n");
     printf("\t| 3. Gestor                         |\n");
+    printf("\t|                                   |\n");
+	printf("\t| 0. Sair                           |\n");
     printf("\t=====================================\n");
-	printf("1. Inserir Meio\n");
-    printf("2. Listar Meios\n");
-    printf("3. Remover Meio\n");
-    printf("4. Guardar Meios\n");
-    printf("5. Ler Meios\n");
-    printf("0. Sair\n");
-    printf("\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\xcd\n");
-    printf("Digite a sua escolha: ");
-    scanf("%d",&op);
-    return(op);
+	printf("\n > Digite a sua escolha: ");
+    scanf("%d",&op_p);
+    return(op_p);
+}
+
+ int menu_meio()
+{
+    int op_meio;
+    system("cls");
+	printf("\n\n\t=====================================\n");
+	printf("\t|    Meio de mobilidade eletrica    |\n");
+	printf("\t=====================================\n");
+	printf("\t| 1. Inserir Meio                   |\n");
+	printf("\t| 2. Remover Meio                   |\n");
+	printf("\t| 3. Listar Meios                   |\n");
+	printf("\t| 4. Guardar Meios                  |\n");
+	printf("\t| 5. Ler Meios                      |\n");
+	printf("\t|                                   |\n");
+	printf("\t| 9. Anterior               0. Sair |\n");
+	printf("\t=====================================\n");
+	printf("\n > Digite a sua escolha: ");
+   	scanf("%d",&op_meio);
+    return(op_meio);
 }
 
 int main()
 {
-    Meio* meios = NULL; // Lista ligada vazia 
-    int op, cod;
+    Meio * meios = NULL; // Lista ligada vazia
+	Cliente * clientes = NULL; // Lista ligada vazia
+	struct Gestor gestor_admin = {"admin", "admin"};
+    int op_p, op_meio, cod, numcliente;
     float bat, aut;
-    char tipo[50];
+    char tipo[50], input_username[20], input_password[20], nome[50], email[50], tel[15], morada[100];
+
     do
     {
-        op = menu();
-        switch(op)
+        op_p = menu_principal();
+        switch(op_p)
         {
-            case 1: 
+			case 1: {
+                do
+                {
+                    op_meio = menu_meio();
+                    switch(op_meio)
+                    {
+                        case 1: printf("Olá está a funcionar"); break;
+                        case 9: menu_principal ();
+                    }
+                }
+                while (op_meio!=0);
+                return 0;
+            }
+            /*case 1: 
                 printf("Codigo?\n");
                 scanf("%d",&cod);
                 scanf("%*c"); 
@@ -53,7 +91,7 @@ int main()
                 printf("Autonomia\n");
                 scanf("%f",&aut);
                 meios = inserirMeio(meios,cod,tipo,bat,aut);
-                break;
+                break;*/
             case 2: listarMeios(meios); break;
             case 3: 
                 printf("Codigo do meio de mobilidade a remover?\n");
@@ -63,9 +101,7 @@ int main()
             case 4: guardarMeios(meios); break;
             case 5: meios = lerMeios(); break;
             case 6: {
-                struct Gestor gestor_admin = {"admin", "admin"};
-                char input_username[20];
-                char input_password[20];
+                
                 printf("Digite seu nome de usuario: ");
                 scanf("%s", input_username);
                 printf("Digite sua senha: ");
@@ -79,5 +115,5 @@ int main()
             }
         }
     }
- while (op!=0);
+    while (op_p!=0);
  }
